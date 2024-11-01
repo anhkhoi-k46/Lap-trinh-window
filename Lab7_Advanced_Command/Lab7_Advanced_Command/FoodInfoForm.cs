368e0d6af8768d12ba29c5af6b384ace1e3efbe4
anhkhoi-k46 <2212396@dlu.edu.vn>
2024-11-01 11:04:48 +0700

    Add files via upload

---
+++ Lab7_Advanced_Command/Lab7_Advanced_Command/FoodInfoForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_Advanced_Command
+{
+    public partial class FoodInfoForm : Form
+    {
+        public FoodInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FoodInfoForm_Load(object sender, EventArgs e)
+        {
+            this.InitValues();
+        }
+
+        private void InitValues()
+        {
+            string connectionString = "server=.;database = RestaurantManagement;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID,Name FROM Category";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            conn.Open();
+            adapter.Fill(ds, "Category");
+
+            cbbCatName.DataSource = ds.Tables["Category"];
+            cbbCatName.DisplayMember = "Name";
+            cbbCatName.ValueMember = "Id";
+            conn.Close();
+            conn.Dispose();
+        }
+        private void ResetText()
+        {
+            txtFoodID.ResetText();
+            txtName.ResetText();
+            txtNotes.ResetText();
+            txtUnit.ResetText();
+            cbbCatName.ResetText();
+            nudPrice.ResetText();
+        }
+
+        private void btnAddFood_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "server=.;database = RestaurantManagement;Integrated Security=true;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "EXUCUTE InsertFood @id OUTPUT,@name,@unit,@FoodCategoryID,@price,@notes";
+
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@name", SqlDbType.NVarChar, 1000);
+                cmd.Parameters.Add("@unit", SqlDbType.NVarChar, 1000);
+                cmd.Parameters.Add("@foodCategoryId", SqlDbType.Int);
+                cmd.Parameters.Add("@price", SqlDbType.Int);
+                cmd.Parameters.Add("@notes", SqlDbType.NVarChar, 3000);
+
+                cmd.Parameters["@name"].Value =txtName.Text;
+                cmd.Parameters["@unit"].Value = txtUnit.Text;
+                cmd.Parameters["@foodCateoryId"].Value = cbbCatName.SelectedValue;
+                cmd.Parameters["@price"].Value = nudPrice.Value;
+                cmd.Parameters["@nontes"].Value = txtNotes.Text;
+
+                conn.Open();
+                int numRowAffected = cmd.ExecuteNonQuery();
+
+               if(numRowAffected > 0)
+                {
+                    String foodID = cmd.Parameters["@id"].Value.ToString();
+                    MessageBox.Show("Succesfully addng new food. Food ID = " + foodID, "Message");
+                    this.ResetText();
+                }
+               else
+                {
+                    MessageBox.Show("Adding Food Failed");
+                }
+                conn.Close();
+                conn.Dispose();
+            }catch (SqlException exception) {
+                MessageBox.Show(exception.Message, "SQL error");
+            }
+                       
+        }
+        public void DisplayFoodInfo(DataRowView rowView)
+        {
+            try
+            {
+                txtFoodID.Text = rowView["ID"].ToString();
+                txtName.Text = rowView["Name"].ToString();
+                txtUnit.Text = rowView["Unit"].ToString();
+                txtNotes.Text = rowView["Notes"].ToString();
+                nudPrice.Text = rowView["Price"].ToString();
+                cbbCatName.SelectedIndex = -1;
+
+                for (int index =0;index < cbbCatName.Items.Count;index++)
+                {
+                    DataRowView cat = cbbCatName.Items[index] as DataRowView;
+                    if (cat["ID"].ToString() == rowView["FoodCategoryID"].ToString())
+                    {
+                        cbbCatName.SelectedIndex = index;
+                        break;
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+                this.Close();
+            }
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                MessageBox.Show("Updating food failed");
+
+                string connectionString = "server =.; database - RestaurantManagement; Integrated Security - true;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "EXECUTE UpdateFood @id, @name, @unit, @foodCategoryID, @price, @notes";
+
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@name", SqlDbType.NVarChar, 1000);
+                cmd.Parameters.Add("@unit", SqlDbType.NVarChar, 100);
+                cmd.Parameters.Add("@foodCategoryId", SqlDbType.Int);
+                cmd.Parameters.Add("@price", SqlDbType.Int);
+                cmd.Parameters.Add("@notes", SqlDbType.NVarChar, 3000);
+
+               
+                cmd.Parameters["@id"].Value = int.Parse(txtFoodID.Text);
+                cmd.Parameters["@name"].Value = txtName.Text;
+                cmd.Parameters["@unit"].Value = txtUnit.Text;
+                cmd.Parameters["@foodCategoryId"].Value = cbbCatName.SelectedValue;
+                cmd.Parameters["@price"].Value = nudPrice.Value;
+                cmd.Parameters["@notes"].Value = txtNotes.Text;
+
+                
+                conn.Open();
+
+                int numRowAffected = cmd.ExecuteNonQuery();
+                
+                if (numRowAffected > 0)
+                {
+                    MessageBox.Show("Successfully updating food", "Message");
+
+                    this.ResetText();
+                }
+                else
+                {
+                    MessageBox.Show("Updating food failed");
+                }
+
+               
+                conn.Close();
+                conn.Dispose();
+            }
+
+           
+            catch (SqlException exception)
+            {
+                MessageBox.Show(exception.Message, "SOL Error");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Lab7_Advanced_Command/Lab7_Advanced_Command/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_Advanced_Command
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.LoadCategory();
+        }
+        private void LoadCategory()
+        {
+            string connectionString = "server=.;database = RestaurantManagement;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID,Name FROM Category";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            conn.Open();
+            adapter.Fill(dt);
+            conn.Close();
+            conn.Dispose();
+
+            cboCategory.DataSource= dt;
+
+            cboCategory.DisplayMember= "Name";
+
+            cboCategory.ValueMember= "ID";
+
+        }
+        private DataTable foodTable;
+        private void cboCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cboCategory.SelectedIndex == -1) { return; }
+            
+            string connectionString = "server=.;database = RestaurantManagement;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Food WHERE FoodCategoryID =@categoryID";
+
+            cmd.Parameters.Add("@categoryID",SqlDbType.Int);
+
+            if(cboCategory.SelectedValue is DataRowView)
+            {
+                DataRowView rowView = cboCategory.SelectedValue as DataRowView;
+                cmd.Parameters["@categoryID"].Value = rowView["ID"];               
+            }
+            else { cmd.Parameters["@categoryID"].Value = cboCategory.SelectedValue; }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            foodTable = new DataTable();
+
+            conn.Open();
+            adapter.Fill(foodTable);
+            
+            conn.Close();
+            conn.Dispose();
+
+            dgvFoodList.DataSource= foodTable;
+            lblQuantity.Text = foodTable.Rows.Count.ToString();
+            lblCatName.Text =cboCategory.Text;
+
+
+        }
+
+        private void tsmCalculateQuantity_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=.;database = RestaurantManagement;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT @numSaleFood = sum(Quantity) FROM BillDetails WHERE FoodID =@foodId";
+
+            if(dgvFoodList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvFoodList.SelectedRows[0];
+
+                DataRowView rowView = selectedRow.DataBoundItem as DataRowView;
+
+                cmd.Parameters.Add("@foodId", SqlDbType.Int);
+                cmd.Parameters["@foodID"].Value = rowView["ID"];
+
+                cmd.Parameters.Add("@numSaleFood",SqlDbType.Int);
+                cmd.Parameters["@numSaleFood"].Direction = ParameterDirection.Output;
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+                string result = cmd.Parameters["@numSaleFood"].Value.ToString();
+                MessageBox.Show("Tổng số lượng món " + rowView["Name"] + " đã bán là: " + result + " " + rowView["Unit"]);
+                conn.Close();
+
+            }
+            cmd.Dispose();
+            conn.Dispose();
+        }
+
+        private void tsmAddFood_Click(object sender, EventArgs e)
+        {
+            FoodInfoForm foodForm = new FoodInfoForm();
+            foodForm.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+            foodForm.Show(this);
+        }
+
+        
+        private void tsmUpdateFood_Click(object sender, EventArgs e)
+        {
+            if(dgvFoodList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvFoodList.SelectedRows[0];
+                DataRowView rowView = selectedRow.DataBoundItem as DataRowView; ;
+                FoodInfoForm foodForm=new FoodInfoForm();
+           
+
+                foodForm.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+                foodForm.Show(this);
+                foodForm.DisplayFoodInfo(rowView);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            int index = cboCategory.SelectedIndex;
+            cboCategory.SelectedIndex = -1;
+            cboCategory.SelectedIndex = index;
+        }
+
+        private void txtSearchByName_TextChanged(object sender, EventArgs e)
+        {
+            if (foodTable == null) return;
+
+            string filterExpression = "Name like '%" + txtSearchByName.Text + "%'";
+            string sortExpression = "Price DESC";
+            DataViewRowState rowStateFilter = DataViewRowState.OriginalRows;
+
+            DataView foodView = new DataView(foodTable, filterExpression, sortExpression, rowStateFilter);
+
+            dgvFoodList.DataSource = foodView;
+        }
+    }
+}
